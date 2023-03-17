9358e301fbde92796ebb4f1a9fd862fc492f8863
maxim-tikhomirov <tvn1212yandex2@gmail.com>
2023-03-17 04:01:15 +0300

    added back button on action bar

--- ExamTask/app/src/main/java/com/example/examtask/MainActivity.kt
+++ ExamTask/app/src/main/java/com/example/examtask/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.examtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        navController = this.findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
 }
